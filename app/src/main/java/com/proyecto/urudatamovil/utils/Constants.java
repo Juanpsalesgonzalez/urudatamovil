8d1df92eb5fee0adb9970319b69d4d2684665293
juanpsalesgonzalez <juanp.sales.gonzalez@gmail.com>
2015-10-03 18:04:58 -0300

    Luego de correr Lint

--- app/src/main/java/com/proyecto/urudatamovil/utils/Constants.java
+++ app/src/main/java/com/proyecto/urudatamovil/utils/Constants.java
@@ -7,8 +7,8 @@ package com.proyecto.urudatamovil.utils;
 public class Constants {
 
   //  private static final String ip_ctr="http://ctr.urudata.com/urudataTEST";
-  //  private static final String ip_ctr="200.71.4.212/urudataTEST";
-    private static final String ip_ctr="192.168.8.100:8080/urudata";
+    private static final String ip_ctr="200.71.4.212/urudataTEST";
+  //  private static final String ip_ctr="192.168.8.100:8080/urudata";
     public static final String URL_CONFIRM="http://" + ip_ctr + "/rest/confirm";
     public static final String URL_OUTBYUSER="http://" + ip_ctr + "/rest/outById";
     public static final String URL_SET_LICENSE="http://" + ip_ctr + "/rest/setLicence";
